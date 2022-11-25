019336432ee61079088d7a25c728efa1d62ff5a2
Jaagrav <jaagrav.seal@atlan.com>
2022-11-25 22:39:13 +0530

    chore: improve code readability

--- src/index.js
+++ src/index.js
@@ -77,39 +77,41 @@ async function getAssetName(octokit, context, fileName, filePath) {
 }
 
 async function createComment(octokit, context, asset, downstreamAssets) {
-  const { pull_request } = context.payload,
-    rows = downstreamAssets.map(
-      ({ displayText, guid, typeName, attributes, meaningNames }) => {
-        const connectorImage = getConnectorImage(attributes.connectorName),
-          certificationImage = attributes?.certificateStatus
-            ? getCertificationImage(attributes?.certificateStatus)
-            : "",
-          readableTypeName = typeName
-            .toLowerCase()
-            .replace(attributes.connectorName, "")
-            .toUpperCase();
+  const { pull_request } = context.payload;
 
-        return [
-          `${connectorImage} [${displayText}](${ATLAN_INSTANCE_URL}/assets/${guid}) ${certificationImage}`,
-          `\`${readableTypeName}\``,
-          attributes?.userDescription || attributes?.description || "--",
-          attributes?.ownerUsers?.join(", ") || "--",
-          meaningNames?.join(", ") || "--",
-        ];
-      }
-    ),
-    comment = `
-## ${getConnectorImage(asset.attributes.connectorName)} [${
-      asset.displayText
-    }](${ATLAN_INSTANCE_URL}/assets/${asset.guid}) ${
-      asset.attributes?.certificateStatus
-        ? getCertificationImage(asset.attributes.certificateStatus)
-        : ""
+  const rows = downstreamAssets.map(
+    ({ displayText, guid, typeName, attributes, meaningNames }) => {
+      const connectorImage = getConnectorImage(attributes.connectorName),
+        certificationImage = attributes?.certificateStatus
+          ? getCertificationImage(attributes?.certificateStatus)
+          : "",
+        readableTypeName = typeName
+          .toLowerCase()
+          .replace(attributes.connectorName, "")
+          .toUpperCase();
+
+      return [
+        `${connectorImage} [${displayText}](${ATLAN_INSTANCE_URL}/assets/${guid}) ${certificationImage}`,
+        `\`${readableTypeName}\``,
+        attributes?.userDescription || attributes?.description || "--",
+        attributes?.ownerUsers?.join(", ") || "--",
+        meaningNames?.join(", ") || "--",
+      ];
     }
+  );
+
+  const comment = `
+## ${getConnectorImage(asset.attributes.connectorName)} [${
+    asset.displayText
+  }](${ATLAN_INSTANCE_URL}/assets/${asset.guid}) ${
+    asset.attributes?.certificateStatus
+      ? getCertificationImage(asset.attributes.certificateStatus)
+      : ""
+  }
 \`${asset.typeName
-      .toLowerCase()
-      .replace(asset.attributes.connectorName, "")
-      .toUpperCase()}\`
+    .toLowerCase()
+    .replace(asset.attributes.connectorName, "")
+    .toUpperCase()}\`
       
 There are ${downstreamAssets.length} downstream asset(s).
 Name | Type | Description | Owners | Terms
@@ -118,12 +120,13 @@ ${rows.map((row) => row.join(" | ")).join("\n")}
 
 ${getImageURL(
   "atlan-logo"
-)} [View asset on Atlan.](${ATLAN_INSTANCE_URL}/assets/${asset.guid})`,
-    commentObj = {
-      ...context.repo,
-      issue_number: pull_request.number,
-      body: comment,
-    };
+)} [View asset on Atlan.](${ATLAN_INSTANCE_URL}/assets/${asset.guid})`;
+
+  const commentObj = {
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: comment,
+  };
 
   if (IS_DEV) return comment;
   return octokit.rest.issues.createComment(commentObj);
